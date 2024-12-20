2ceb88c7141fe43132390a08c54d2c2f0c697162
Ahmad Barish <ahmad.bariish@gmail.com>
2024-12-20 23:50:43 +0300

    Add project files.

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BinarySearch
+{
+    internal class Program
+    {
+        static int binarySearch(int[] arr, int item)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if(arr[mid] == item) return mid;
+                if (arr[mid] < item) left = mid + 1;
+                else right = mid - 1; 
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"This is target {binarySearch([1, 3, 6], 6)}");
+        }
+    }
+}
